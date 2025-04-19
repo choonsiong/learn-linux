b6598691ea76ca91fc8b7b5c08232d5d19805ec9
choonsiong <choonsiong.lee@gmail.com>
2025-04-20 04:34:37 +0800

    Auto commit

--- demo/stripcomments.sh
+++ demo/stripcomments.sh
@@ -2,4 +2,5 @@
 
 # Eliminates all lines that starts with the # or ; characters or are completely empty
 # ^[[:space:]]*# pattern finds lines starting with #, where there may be any number of spaces and tab characters between the start of the line (^) and #
+# ^$ pattern applies to empty lines consisting only of the beginning and end of line ($)
 grep -Ev '^[[:space:]]*#|^[[:space:]]*;|^$' $1
