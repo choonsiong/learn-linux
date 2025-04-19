5e28bfb5c34e45d807bcc164ee14ca3b0135e520
choonsiong <choonsiong.lee@gmail.com>
2025-04-20 04:27:07 +0800

    Auto commit

--- demo/grepall.sh
+++ demo/grepall.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# -mtime 7 find files modified exactly 7 days ago
+# {} is a placeholder for the current file
+# \; ends the -exec clause, the semicolon must be escaped (\;) or quoted (';') so the shell doesn't interpret it
 find . -type f -mtime 7 -exec grep -q $1 {} \; -print
